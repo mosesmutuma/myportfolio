16753d7fa2264c1e988a0e1a45fcbd9e677eb733
Moses Mutuma <mosesmutuma709@gmail.com>
2025-07-31 13:08:03 +0300

    Update urls.py to serve media files

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5s#1w%*0bk4y@nc)(-0o7pe_rbyl9n^j)4$9^^))2apta6rjb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mosesmutuma.onrender.com', 'localhost', '127.0.0.1']
 
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio', 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ safer and standard
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
