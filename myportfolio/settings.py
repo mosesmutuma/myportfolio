365743aaafc34a75b786b73b4809b1d1efe7b9a9
Moses Mutuma <mosesmutuma709@gmail.com>
2025-08-01 17:36:19 +0300

    add all

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5s#1w%*0bk4y@nc)(-0o7pe_rbyl9n^j)4$9^^))2apta6rjb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mosesmutuma.onrender.com', 'localhost', '127.0.0.1']
 
